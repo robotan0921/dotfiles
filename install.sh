77ada899e2b8fbf07d4c8126bf8abb27dfefde49
Takuro Yamamoto <robotan0921@gmail.com>
2019-07-03 09:53:55 +0900

    Update .zshrc and install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-DOT_FILES=(.bashrc .bash_profile .vimrc .gvimrc)
+DOT_FILES=(
+    .zshrc
+    .bashrc
+    .bash_profile
+    .vimrc
+    .gvimrc
+    .git_templates
+)
 
 for file in ${DOT_FILES[@]}
 do
